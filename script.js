6f954f1d5e9e1d5b294ede47ec1fd03ff7c90fd2
aramedis <119070223+aramedis@users.noreply.github.com>
2023-03-14 02:05:41 +0000

    colour code functionality

--- script.js
+++ script.js
@@ -16,13 +16,31 @@ var timeblockArr = [
     "5pm",
     "6pm"];
 
-timeblockArr.forEach(element => {
+timeblockArr.forEach((time, index) => {
     let row = $("<div>").addClass("row tBlock");
-    let hour = $("<div>").addClass("col-1 hour").text(element);
+    let hour = $("<div>").addClass("col-1 hour").text(time);
     let text = $("<textarea>").addClass("col-10 text");
     var saveBtn = $("<button>").addClass("col-1 saveBtn").text("save");
 
     //append components to html
     row.append(hour, text, saveBtn);
     $(".container").append(row);
+
+
+    //Timecheck functionality
+    let currentTime = moment().hour();
+    let tBlockTime = index +8;
+    console.log ("index is " +index , "and tBlockTime is" , tBlockTime)
+
+    //add colour class dependent on time
+    if (currentTime > tBlockTime) {
+        text.addClass("past");
+    } else if (currentTime === tBlockTime) {
+        text.addClass("present");
+    } else {
+        text.addClass("future");
+    }
+
 });
+
+
